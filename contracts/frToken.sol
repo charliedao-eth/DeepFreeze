059a84908ba917193d1567ff22e90673bf2c09a9
chalex-eth <43524913+chalex-eth@users.noreply.github.com>
2022-04-19 10:13:55 +0200

    Update frToken.sol

--- contracts/frToken.sol
+++ contracts/frToken.sol
@@ -2,14 +2,13 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title frToken
 /// @author chalex.eth - CharlieDAO
 /// @notice frToken contract
 
-contract frToken is ERC20, ERC20Burnable, Ownable {
+contract frToken is ERC20, Ownable {
     address public governorAddress;
     bool private isInitialized;
 
