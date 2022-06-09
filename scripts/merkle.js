28f791754a4bf96bbf94c46ef307766f7baa51af
chalex-eth <43524913+chalex-eth@users.noreply.github.com>
2022-06-09 09:23:54 +0200

    update

--- scripts/merkle.js
+++ scripts/merkle.js
@@ -1,29 +1,40 @@
 const ethers = require("ethers");
 const { MerkleTree } = require('merkletreejs');
 const keccak256 = require('keccak256')
+const fs = require('fs');
 
 
-const users = require('./airdrop_needs1e18_formatted.json');
+const users = require('./airdrop_has1e18_strings.json');
 
 // equal to MerkleDistributor.sol #keccak256(abi.encodePacked(account, amount));
 const elements = users.map((x) =>
     ethers.utils.solidityKeccak256(["uint256", "address", "uint256"], [x.index, x.address, x.amount])
 );
 
-const merkleTree = new MerkleTree(elements, keccak256, { sort: true });
+var merkleTree = new MerkleTree(elements, keccak256, { sort: true });
 
-const root = merkleTree.getHexRoot();
+var root = merkleTree.getHexRoot();
 
-const leaf = elements[0]; // To get the proof of the address change the idx of elements
-const proof = merkleTree.getHexProof(leaf);
+var leaf = elements[1]; // To get the proof of the address change the idx of elements
+var proof = merkleTree.getHexProof(leaf);
 console.log(root);
 console.log(proof)
 
-const merkleJsonExport = users.map(function (user, index) {
-    return Object.assign({}, user, {
+var merkleJsonExport = users.map(function (user, index) {
+    const userObj = Object.assign({}, user, {
         amount: user.amount.toString(),
         merkleProof: merkleTree.getHexProof(elements[index])
     })
+    console.log(userObj);
+    return userObj;
 });
+merkleJsonExport = JSON.stringify(merkleJsonExport);
 
-console.log(JSON.stringify(merkleJsonExport));
+
+try {
+    fs.writeFileSync('./users_export.json', merkleJsonExport);
+    // file written successfully
+    console.log("done");
+} catch (err) {
+    console.error(err);
+}
